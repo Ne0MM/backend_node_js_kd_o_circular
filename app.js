b511c311dd032605af68ee588d2642de382215e2
ne0mm <pablomaximiliano260@gmail.com>
2024-11-04 18:27:45 -0300

    Only consoles the number of connected users

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
-import { dirname, join } from 'node:path'
+import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import os from 'os';
 
@@ -10,65 +10,71 @@ const server = createServer(app);
 const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+let connectedClients = 0; // Track the number of connected clients
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log("user has connected");
+  connectedClients++;
+  console.log("User has connected");
+
+  socket.on('disconnect', () => {
+    connectedClients--;
+    console.log("User has disconnected");
+  });
 });
 
-io.on('disconnect', (socket) => {
-  console.log("user has disconnected");
-})
-
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('Server running at http://localhost:3000');
 });
 
 const getMemoryUsage = () => {
-    const totalMemory = os.totalmem();
-    const freeMemory = os.freemem();
-    const usedMemory = totalMemory - freeMemory;
-    const memoryUsagePercentage = (usedMemory / totalMemory) * 100;
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsagePercentage = (usedMemory / totalMemory) * 100;
 
-    return {
-        totalMemory: (totalMemory / 1024 / 1024).toFixed(2) + ' MB',
-        usedMemory: (usedMemory / 1024 / 1024).toFixed(2) + ' MB',
-        freeMemory: (freeMemory / 1024 / 1024).toFixed(2) + ' MB',
-        memoryUsagePercentage: memoryUsagePercentage.toFixed(2) + '%'
-    };
-}
+  return {
+    totalMemory: (totalMemory / 1024 / 1024).toFixed(2) + ' MB',
+    usedMemory: (usedMemory / 1024 / 1024).toFixed(2) + ' MB',
+    freeMemory: (freeMemory / 1024 / 1024).toFixed(2) + ' MB',
+    memoryUsagePercentage: memoryUsagePercentage.toFixed(2) + '%',
+  };
+};
 
 const getCpuUsage = () => {
-    const cpus = os.cpus();
-    let totalIdle = 0, totalTick = 0;
+  const cpus = os.cpus();
+  let totalIdle = 0,
+    totalTick = 0;
 
-    cpus.forEach(cpu => {
-        for (const type in cpu.times) {
-            totalTick += cpu.times[type];
-        }
-        totalIdle += cpu.times.idle;
-    });
+  cpus.forEach((cpu) => {
+    for (const type in cpu.times) {
+      totalTick += cpu.times[type];
+    }
+    totalIdle += cpu.times.idle;
+  });
 
-    const idle = totalIdle / cpus.length;
-    const total = totalTick / cpus.length;
+  const idle = totalIdle / cpus.length;
+  const total = totalTick / cpus.length;
 
-    const usagePercentage = ((total - idle) / total) * 100;
+  const usagePercentage = ((total - idle) / total) * 100;
 
-    return {
-        cpuUsagePercentage: usagePercentage.toFixed(2) + '%'
-    };
-}
+  return {
+    cpuUsagePercentage: usagePercentage.toFixed(2) + '%',
+  };
+};
 
-const logSystemUsage = () =>{
-
+const logSystemUsage = () => {
   const memoryUsage = getMemoryUsage();
   const cpuUsage = getCpuUsage();
 
-  console.log(memoryUsage.memoryUsagePercentage);
-  console.log(cpuUsage.cpuUsagePercentage);
-}
+  console.clear();
+  console.log(`Connected clients: ${connectedClients}`);
+  console.log(`Memory Usage: ${memoryUsage.memoryUsagePercentage}`);
+  console.log(`CPU Usage: ${cpuUsage.cpuUsagePercentage}`);
+};
 
 setInterval(logSystemUsage, 1000);
+
